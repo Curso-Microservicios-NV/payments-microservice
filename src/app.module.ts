14e7b0f24f558959ff7cd405622cb42b6c24263a
Norberto Vega <norberto.a.vega@gmail.com>
2025-07-02 00:24:19 -0300

    Add HealthCheck module and controller for service health monitoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PaymentModule } from './payment/payment.module';
+import { HealthCheckModule } from './health-check/health-check.module';
 
 
 @Module({
-  imports: [PaymentModule],
+  imports: [
+    PaymentModule,
+    HealthCheckModule
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
